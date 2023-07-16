cb0364b8243f4fd683056956299721f06ae31e98
tamisuhiko <nhatlinh_216@yahoo.com>
2023-07-16 08:15:03 +0700

    update code

--- src/Wpf.Ui.Main/ViewModels/MainWindowViewModel.cs
+++ src/Wpf.Ui.Main/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,4 @@
-﻿// This Source Code Form is subject to the terms of the MIT License.
+// This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
@@ -33,7 +33,7 @@ public partial class MainWindowViewModel : ObservableObject
 
     private void InitializeViewModel()
     {
-        ApplicationTitle = "WPF UI - MVVM Demo";
+        ApplicationTitle = "Qorvo Copyright";
 
         NavigationItems = new ObservableCollection<object>
         {
